0928eb8e79ad72463aacaa0bbced3193a402859b
jarek104 <jaroslawolewniczak@Jaroslaws-MacBook-Pro.local>
2016-10-18 23:27:07 -0400

    added more data

--- script.js
+++ script.js
@@ -20,16 +20,19 @@ var MyStudents =
         "Major" : "Biotechnology"
         }
         
-    ]
+    ],
+    "classroom" : 104,
+    "instructorName" : "Brian Griffin"
 
 }
 
 $( document ).ready(function() {
-    var stringBuilder = "We have the following students:<br>";
+    var stringBuilder = "We have the following students in the class:<br>";
     for (var i = 0; i < MyStudents.students.length;i++) {
         stringBuilder += MyStudents.students[i].firstName + " " + MyStudents.students[i].lastName + " is " + MyStudents.students[i].Age + " years old and studies "+ MyStudents.students[i].Major + "<br>";
     }
-    stringBuilder += "There is a total of " + MyStudents.students.length + " students.";
+    stringBuilder += "There is a total of " + MyStudents.students.length + " students.<br>";
+    stringBuilder += "The class takes places in classroom number " + MyStudents.classroom + " and the instructor name is " + MyStudents.instructorName + ".";
     $("#demo").html(stringBuilder);
 });
 
