0a86f55fad2d9903ffcb3862708008716d99b669
jarek104 <jaroslawolewniczak@Jaroslaws-MacBook-Pro.local>
2016-10-18 23:21:00 -0400

    done

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-Country = function(name, continent, capital, population){
-    this.Name = name;
-    this.Continent = continent;
-    this.Capital = capital;
-    this.PopulationInMillions = population;
-}
-Country.prototype.DisplayInfo = function(){
-    return this.Name + " is a country in " + this.Continent +
-     " with the capital of " + this.Capital + " and the total population of " + 
-     this.PopulationInMillions.toString() + " millions.";
-}
-Country.prototype.ComparePopulation = function(Country) {
-    var biggerPopulationCountry;
-    var text = "Comparing population of " + this.Name + " and " + Country.Name + "<br/>";
-    text += "Country with a larger population is ";
-    if (this.PopulationInMillions > Country.PopulationInMillions){
-        text += this.Name;
-    }
-    else {
-        text += Country.Name; 
-    }
-    return text;    
+var MyStudents = 
+{
+    "students" : [
+        {
+        "firstName" : "Lois", 
+        "lastName" : "Griffin", 
+        "Age" : 35,
+        "Major" : "Psychotherapy"
+        },
+        {
+        "firstName" : "Peter", 
+        "lastName" : "Griffin", 
+        "Age" : 40,
+        "Major" : "Political Science"
+        },
+        {
+        "firstName" : "Stewie", 
+        "lastName" : "Griffin", 
+        "Age" : 5,
+        "Major" : "Biotechnology"
+        }
+        
+    ]
+
 }
 
-var USA = new Country("United States of America", "North America", "Washington", 325);
-var Poland = new Country("Poland", "Europe", "Warsaw", 39);
-var Japan = new Country("Japan", "Asia", "Tokyo", 126);
-var Italy = new Country("Italy", "Europe", "Rome", 60);
+$( document ).ready(function() {
+    var stringBuilder = "We have the following students:<br>";
+    for (var i = 0; i < MyStudents.students.length;i++) {
+        stringBuilder += MyStudents.students[i].firstName + " " + MyStudents.students[i].lastName + " is " + MyStudents.students[i].Age + " years old and studies "+ MyStudents.students[i].Major + "<br>";
+    }
+    stringBuilder += "There is a total of " + MyStudents.students.length + " students.";
+    $("#demo").html(stringBuilder);
+});
 
-document.body.innerHTML = Poland.DisplayInfo();
-document.body.innerHTML += "<br/><br/>";
-document.body.innerHTML += USA.DisplayInfo();
-document.body.innerHTML += "<br/><br/>";
-document.body.innerHTML += USA.ComparePopulation(Japan);
-document.body.innerHTML += "<br/><br/>";
-document.body.innerHTML += Poland.ComparePopulation(Italy);
